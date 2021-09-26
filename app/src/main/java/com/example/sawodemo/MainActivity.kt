a8f98859c60aa6b5a58125d68b4accd971a35003
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2021-09-26 23:13:32 +0530

    Encryption protected

--- app/src/main/java/com/example/sawodemo/MainActivity.kt
+++ app/src/main/java/com/example/sawodemo/MainActivity.kt
@@ -16,8 +16,8 @@ class MainActivity : AppCompatActivity() {
         binding.loginBtn.setOnClickListener{
             Sawo(
                 this,
-                "7b9293df-8432-4a59-907f-01db93d559aa", // your api key
-                "614cdb0dec39c0c001ec5cecgIuTa6WbQSKLCZVShF4bioR8"  // your api key secret
+                "", // your api key
+                ""  // your api key secret
             ).login(
                 "email", // can be one of 'email' or 'phone_number_sms'
                 HomeActivity::class.java.name // Callback class name
